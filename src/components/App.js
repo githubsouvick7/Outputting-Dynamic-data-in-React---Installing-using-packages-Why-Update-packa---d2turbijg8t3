af6be576b6dfa3d866c25469ea3181ba54ac3adc
Souvick Chakraborty <114563269+githubsouvick7@users.noreply.github.com>
2023-01-13 22:09:25 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,22 @@
-import React,{useState,useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import '../styles/App.css';
 const App = () => {
-//code here
+  //code here
+  const [name, setName] = useState("");
+
+  function handleChange(event) {
+    setName(event.target.value);
+  }
+  function handleClick() {
+    // Update the name in the paragraph tag
+    document.getElementById("add").innerHTML = name;
+  }
+
   return (
     <div id="main">
-      <input id='input' onChange={}></input>
-      <button id='button' onClick={}>Click</button>
-      <p id='text'> Hello my name is ____ and I study at Newton School</p>
+      <input id='input' onChange={handleChange}></input>
+      <button id='button' onClick={handleClick}>Click</button>
+      <p id='text'> Hello my name is <span id='add'>____</span> and I study at Newton School</p>
     </div>
   )
 }
